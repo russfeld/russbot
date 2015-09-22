149f866352830594925de86d8a45e2c462a91801
Richard Petrie <rap1011@ksu.edu>
2015-09-21 21:08:28 -0500

    Don't catch single character keys and block ku++
    Untested

--- src/main/java/russbot/plugins/Scorekeeping.java
+++ src/main/java/russbot/plugins/Scorekeeping.java
@@ -64,7 +64,7 @@ public class Scorekeeping implements Plugin, Runnable {
 
     @Override
     public String getRegexPattern() {
-        return "((^([\\w]+)((\\+\\+)|(--))\\z)|(^!score[s]?\\z))";
+        return "((^([\\w]{2,})((\\+\\+)|(--))\\z)|(^!score[s]?\\z))";
     }
 
     @Override
@@ -140,13 +140,19 @@ public class Scorekeeping implements Plugin, Runnable {
             }
             String change = "";
             if(message.charAt(message.length() - 1) == '+'){
-                data.put(key, data.get(key) + 1);
-                change = "gained";
+                if(!key.toLowerCase().equals("ku") { //TODO remove .toLowerCase() after case checking is implemented
+                    data.put(key, data.get(key) + 1);
+                    change = "gained";
+                }
             }else{
                 data.put(key, data.get(key) - 1);
                 change = "lost";
             }
+            if(key.toLowerCase().equals("ku") { //TODO remove .toLowerCase() after case checking is implemented
+                Session.getInstance().sendMessage("KU may only lose points.");
+            }else{
             Session.getInstance().sendMessage(key + " has " + change + " a point for a total of " + data.get(key) + " points", channel);
+            }
             try {
                 oos.writeObject(data);
                 oos.flush();
