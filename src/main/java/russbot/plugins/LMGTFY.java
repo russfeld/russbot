a4beb7d2fc437f14ccd34fff03e10f9351057a88
at0dd <alex@atodd.io>
2016-12-22 21:41:41 -0600

    Add Wolfram API Search Plugin

--- src/main/java/russbot/plugins/LMGTFY.java
+++ src/main/java/russbot/plugins/LMGTFY.java
@@ -33,8 +33,8 @@ public class LMGTFY implements Plugin {
     @Override
     public String[] getCommands(){
         String[] commands = {
-            "!lmgtfy [string] - Get a link to Google something.",
-            "!lmgtfye [string] - Get a link to Google something and explain how to use the internet."
+            "!lmgtfy <string> - Get a link to Google something.",
+            "!lmgtfye <string> - Get a link to Google something and explain how to use the internet."
         };
         return commands;
     }
